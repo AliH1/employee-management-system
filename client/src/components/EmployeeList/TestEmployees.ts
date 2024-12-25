995cac0455a9a3d45dd7f808e7ff734812217431
Ali Hassan <ahassan0213@gmail.com>
2024-12-24 20:05:28 -0500

    changed name of file

--- client/src/components/EmployeeList/TestEmployees.ts
+++ client/src/components/EmployeeList/TestEmployees.ts
@@ -1,4 +1,4 @@
-export const employeeList = [
+export const List = [
   {name: 'John Doe', email: 'Jhon@gmail.com', phone: '000-000-000', department: 'IT', position: 'full stack developer', status: 'active'},
   {name: 'John Doe', email: 'Jhon@gmail.com', phone: '000-000-000', department: 'IT', position: 'full stack developer', status: 'active'},
   {name: 'John Doe', email: 'Jhon@gmail.com', phone: '000-000-000', department: 'IT', position: 'full stack developer', status: 'active'},
